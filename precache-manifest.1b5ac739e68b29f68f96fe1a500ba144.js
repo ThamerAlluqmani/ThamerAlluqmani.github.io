6cad61f55285367b36aaa9e80969fb2cdc46d788
Thamer <thamer.allogmani@gmail.com>
2022-06-16 22:34:26 +0300

    Updates

---
+++ precache-manifest.1b5ac739e68b29f68f96fe1a500ba144.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8130353566b6c6080f8a71299882018d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "766bba36a5fd211bd2c1",
+    "url": "/static/css/3.5efbcdfd.chunk.css"
+  },
+  {
+    "revision": "a3dea10b3161e69dc0ab",
+    "url": "/static/css/4.9af31e70.chunk.css"
+  },
+  {
+    "revision": "1df50576f985df752421",
+    "url": "/static/css/main.7b2cb803.chunk.css"
+  },
+  {
+    "revision": "044599802fe2ff4f20fc",
+    "url": "/static/js/2.55386862.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.55386862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "766bba36a5fd211bd2c1",
+    "url": "/static/js/3.432080f7.chunk.js"
+  },
+  {
+    "revision": "a3dea10b3161e69dc0ab",
+    "url": "/static/js/4.f8bbceb2.chunk.js"
+  },
+  {
+    "revision": "1df50576f985df752421",
+    "url": "/static/js/main.e354a160.chunk.js"
+  },
+  {
+    "revision": "369d762acd0371086a3b",
+    "url": "/static/js/runtime-main.79474a34.js"
+  },
+  {
+    "revision": "f0ae36efb2ba9352a5e4c5b586ddb63f",
+    "url": "/static/media/1625002574550.f0ae36ef.jfif"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "b42276aa88f4da9504d586db38eea793",
+    "url": "/static/media/Semi2.b42276aa.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "d1c71700b7761b59a40d1c875d733d10",
+    "url": "/static/media/edhhah.d1c71700.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "c629459395c082b6f5d84d3e63b0ec56",
+    "url": "/static/media/the-ciacon.c6294593.png"
+  }
+]);
